5e53c949f87f95b0b7e7aead6e12df45a4bb4c65
Antonio Muñiz <amuniz@cloudbees.com>
2016-03-31 15:43:13 +0200

    Seville JAM

---
+++ SEVILLE_JAM.md
@@ -0,0 +1,35 @@
+# Initial Environment
+
+1. Jenkins
+1. Pipeline Plugin
+1. Pipeline Stage View Plugin
+1. Checkstyle Plugin
+1. Findbugs Plugin
+1. PMD Plugin
+1. SSH Agent
+1. Two agents: linux1, linux2
+1. This repository
+
+#Continuous Deployment Pipeline
+
+### Compile
+
+mvn clean install -DskipTest
+
+### Test
+
+1. linux1: mvn test -Diterations=5
+1. linux2: mvn test -Diterations=10
+
+Fail fast: do not wait for one branch to finish if the other one already failed.
+
+### Code Quality
+
+Checkstyle, Findbugs, PMD
+
+### Deploy
+
+1. Only one simultaneous deploy.
+1. Wait human interaction before deploy.
+1. Ask where to deploy.
+1. Simulated by copying the JAR to localhost by scp.
--- src/test/java/com/cloudbees/manticore/AppTest.java
+++ src/test/java/com/cloudbees/manticore/AppTest.java
@@ -7,32 +7,36 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
-     * @param testName name of the test case
+     * @param testName
+     *            name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws InterruptedException {
+        Integer it = Integer.getInteger("iterations");
+        if (it != null) {
+            for (int i = 0; i < it; i++) {
+                Thread.sleep(1000);
+                System.out.println("Iteration " + i);
+            }
+            return;
+        }
+        assertTrue(false);
     }
 }
